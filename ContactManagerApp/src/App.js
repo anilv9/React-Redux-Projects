5cd9777cfb220793034040c48c6d20c96522c00f
Anil Vemula <anilvemula@Anils-MBP.fios-router.home>
2020-10-24 22:44:23 -0400

    created header component

--- ContactManagerApp/src/App.js
+++ ContactManagerApp/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>The function component</h1>
+      <Header branding="Contact Manager"/>
+      <Contact name="John Doe" email="jdoe@gmail.com" phone="987-354-7463"/>
+      <Contact name="Anil vem" email="anil@gmail.com" phone="987-222-7463"/>
     </div>
   );
 }
---
+++ ContactManagerApp/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+  const {branding} = props;
+  return (
+    <div>
+      <h1>{branding}</h1>
+    </div>
+  )
+}
+Header.defaultProps = {
+  branding: 'My App'
+}
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+}
+
+export default Header;
